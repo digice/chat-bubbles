2d209b9183b73b66641fadc2cf2ca9335135cbe6
Roderic Linguri <linguri@digices.com>
2018-01-20 16:22:56 -0700

    Create Project from Single View App Template

---
+++ chat-bubbles/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  chat-bubbles
+//
+//  Created by Roderic Linguri <linguri@digices.com>
+//  Copyright © 2018 Digices. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
